45c366f8979cf9c0d8855a363a33fe759df41df2
Cesar Morales Diaz <camd.jun@gmail.com>
2017-08-22 16:58:36 -0600

    First commit project Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app') 
+ 
+@section('content')
+<div class="jumbotron text-center">
+    <h1>Laratter</h1>
+    <nav>
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/acerca">Acerca de nosotros</a></li>
+        </ul>
+    </nav>
+</div>
+@endsection
