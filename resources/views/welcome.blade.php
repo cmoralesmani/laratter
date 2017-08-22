056f5923df1fea0840d7cc0fcd4f07ef6d9984b1
Cesar Morales Diaz <camd.jun@gmail.com>
2017-08-22 17:19:06 -0600

    Agregado de Boostrap para las vistas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,20 @@
     <nav>
         <ul class="nav nav-pills">
             <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="/acerca">Acerca de nosotros</a></li>
         </ul>
     </nav>
+</div>
+<div class="row">
+    @forelse($messages as $message)
+        <div class="col-6">
+            <img class="img-thumbnail" src="{{$message['image']}}" />
+            <p calss="card-text">
+                {{ $message['content'] }}
+                <a href="/messages/{{ $message['id'] }}">Leer más</a>
+            </p>
+        </div>
+    @empty
+        <p>No hay mensajes destacados.</p>
+    @endforelse
 </div>
 @endsection
